981d62cb684ce0a4d6f594c231040f6811c6a5db
Kornelija Gasparaityte <kornelija.gasparaityte@gmail.com>
2021-04-28 19:59:07 +0300

    Initial commit

---
+++ PT34_Data_Processing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT34_Data_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
